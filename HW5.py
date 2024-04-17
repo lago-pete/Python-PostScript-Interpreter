7112ad215134f5d4d927b2118ef90159cb92ec58
lago-pete <lagonegropeter@gmail.com>
2024-04-16 18:05:40 -0700

    Finished

--- HW5.py
+++ HW5.py
@@ -1,16 +1,349 @@
 #Peter Lagonegro
 
-from HW5_skeleton import *
+
+
+import re
+
 
 opstack = []  
 dictstack = []  
 
+
+
+
+commands = ['opPop','opPush','dictPop','dictPush','define', 'lookup', 'add', 'sub', 'mul', 'div','mod','eq', 'lt', 'gt', 'length', 'get', 'getinterval', 'put', 'dup', 'exch', 'pop', 'copy', 'clear', 'stack','roll', 'dict', 'begin', 'end', 'def', 'if', 'ifelse', 'for']
+
+
+
+def tokenize(s):
+    return re.findall("/?[a-zA-Z()][a-zA-Z0-9_()]*|[-]?[0-9]+|[}{]+|%.*|[^ \t\n]", s)
+
+
+# complete this function
+# The it argument is an iterator.
+# The sequence of return characters should represent a list of properly nested
+# tokens, where the tokens between '{' and '}' is included as a sublist. If the
+# parenteses in the input iterator is not properly nested, returns False.
+def groupMatching2(it):
+    res = []
+    for c in it:
+        if c == '}':
+            return res
+        elif c=='{':
+            # Note how we use a recursive call to group the tokens inside the
+            # inner matching parenthesis.
+            # Once the recursive call returns the code array for the inner
+            # paranthesis, it will be appended to the list we are constructing
+            # as a whole.
+            res.append(groupMatching2(it))
+        elif c.isdigit():
+            c = int(c)
+            res.append(c)
+        elif isinstance(c, str):
+            if c == 'True':
+                c = True
+                res.append(c)
+            elif c == 'False':
+                c = False
+                res.append(c)
+            else:
+                c = str(c)
+                res.append(c)
+        else:
+            res.append(c)
+    return False
+
+
+# Complete this function
+# Function to parse a list of tokens and arrange the tokens between { and } braces
+# as code-arrays.
+# Properly nested parentheses are arranged into a list of properly nested lists.
+def parse(L):
+    res = []
+    it = iter(L)
+    for c in it:
+        if c=='}':  #non matching closing paranthesis; return false since there is
+                    # a syntax error in the Postscript code.
+            return False
+        elif c=='{':
+            res.append(groupMatching2(it))
+            
+        elif c.isdigit():
+            c = int(c)
+            res.append(c)
+        elif isinstance(c, str):
+            if c == 'True':
+                c = True
+                res.append(c)
+            elif c == 'False':
+                c = False
+                res.append(c)
+            else:
+                c = str(c)
+                res.append(c)
+        else:
+            res.append(c)
+    return res
+
+
+# Write the necessary code here; again write
+# auxiliary functions if you need them. This will probably be the largest
+# function of the whole project, but it will have a very regular and obvious
+# structure if you've followed the plan of the assignment.
+#
+
+
+def interpretSPS(code):
+    count = 0
+    for token in code:
+        count += 1
+        if isinstance(token,str) and token.startswith('/') and isinstance(code[count],list):
+            opPush(token)
+            opPush(code.pop(count))
+            continue
+        if isinstance(token, list):  # It's a code array, handle nested blocks
+            opPush(token)
+        elif isinstance(token, str):
+            if isinstance(token,str) and token.startswith('/'):
+                opPush(token)
+                continue
+            if lookup(token):
+                continue
+            elif token in commands:
+                commanding(token)
+                continue
+            else:
+                opPush(token)
+        else:
+            opPush(token)
+          
+    
+
+
+def commanding(token):
+    if token in commands:
+        if token == 'opPop':
+            opPop()
+            return 
+        elif token == 'opPush':
+            opPush()
+            return
+        elif token == 'dictPop':
+            dictPop()
+            return
+        elif token == 'dictPush':
+            dictPush()
+            return
+        elif token == 'define':
+            define()
+            return
+        elif token == 'lookup':
+            lookup()
+            return
+        elif token == 'add':
+            add()
+            return
+        elif token == 'sub':
+            sub()
+            return
+        elif token == 'mul':
+            mul()
+            return
+        elif token == 'div':
+            div()
+            return
+        elif token == 'mod':
+            mod()
+            return
+        elif token == 'eq':
+            eq()
+            return
+        elif token == 'lt':
+            lt()
+            return
+        elif token == 'gt':
+            gt()
+            return
+        elif token == 'length':
+            length()
+            return
+        elif token == 'get':
+            get()
+            return
+        elif token == 'getinterval':
+            getinterval()
+            return
+        elif token == 'put':
+            put()
+            return
+        elif token == 'dup':
+            dup()
+            return
+        elif token == 'exch':
+            exch()
+            return
+        elif token == 'pop':
+            pop()
+            return
+        elif token == 'copy':
+            copy()
+            return
+        elif token == 'clear':
+            clear()
+            return
+        elif token == 'stack':
+            stack()
+            return
+        elif token == 'roll':
+            roll()
+            return
+        elif token == 'dict':
+            psDict()
+            return
+        elif token == 'begin':
+            begin()
+            return
+        elif token == 'end':
+            end()
+            return
+        elif token == 'def':
+            psDef()
+            return
+        elif token == 'if':
+            psIf()
+            return
+        elif token == 'ifelse':
+            psIfelse()
+            return
+        elif token == 'for':
+            psFor()
+            return
+
+
+
+#-------------------------Helper Functions------------------------------------      
+            
+            
+def psIf():
+    block = opPop()
+    condition = opPop()
+    if condition:
+        interpretSPS(block)
+
+def psIfelse():
+    false_block = opPop() 
+    
+    true_block = opPop()
+    
+    condition = opPop()
+    
+    if condition:
+        interpretSPS(true_block)
+    else:
+        interpretSPS(false_block)
+
+def psFor():
+    block = opPop()  
+    end = int(opPop())  
+    
+    increment = int(opPop())      
+    start = int(opPop())  
+    
+
+    for i in range(start, end-1 , increment):
+        opPush(i)
+        interpretSPS(block)
+
+
+
+
+
+#testing
+input1 = """
+        /square {
+               dup mul
+        } def
+        (square)
+        4 square
+        dup 16 eq
+        {(pass)} {(fail)} ifelse
+        stack
+        """
+
+input2 ="""
+    (facto) dup length /n exch def
+    /fact {
+        0 dict begin
+           /n exch def
+           n 2 lt
+           { 1}
+           {n 1 sub fact n mul }
+           ifelse
+        end
+    } def
+    n fact stack
+    """
+
+input3 = """
+        /fact{
+        0 dict
+                begin
+                        /n exch def
+                        1
+                        n -1 1 {mul} for
+                end
+        } def
+        6
+        fact
+        stack
+    """
+
+input4 = """
+        /lt6 { 6 lt } def
+        1 2 3 4 5 6 4 -3 roll
+        dup dup lt6 {mul mul mul} if
+        stack
+        clear
+    """
+
+input5 = """
+        (CptS355_HW5) 4 3 getinterval
+        (355) eq
+        {(You_are_in_CptS355)} if
+         stack
+        """
+
+input6 = """
+        /pow2 {/n exch def
+               (pow2_of_n_is) dup 8 n 48 add put
+                1 n -1 1 {pop 2 mul} for
+              } def
+        (Calculating_pow2_of_9) dup 20 get 48 sub pow2
+        stack
+        """
+   
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#------------------------------------------------------------------------------
+
+
 def opPop():
     if not opstack:
-        print("stack is empty")
         return False
     else:
-        print("successfully popped")
         return opstack.pop()
     
 
@@ -20,14 +353,13 @@ def opPush(value):
     
 def dictPop():
     if not dictstack:
-        print("dictionary empty bozo")
         return 0
     else:
         return dictstack.pop()
     
 
 def dictPush(d):
-    dictstack.append(d)
+   dictstack.append(d)
     
 
 def define(name, value):
@@ -41,21 +373,28 @@ def define(name, value):
     
 def lookup(name):
     if not isinstance(name, str):
-        print("No name was entered")
         return False
     else:
         if name[0] != '/':
             name = '/' + name
         for dic in reversed(dictstack):
             if name in dic:
-                return dic[name]
-        print("Incorrect Name Entered")
+                if isinstance(dic[name], list):
+                    temp = dic[name].copy()
+                    interpretSPS(temp)
+                    return True 
+                else:
+                    temp = [dic[name]]
+                    interpretSPS(temp)
+                    return True
+                   
+        
         return False
 
     
 def add():
     if len(opstack) < 2:
-        print("Not enough Arguements")
+        
         return False
     arg1 = opstack.pop()
     arg2 = opstack.pop()
@@ -63,15 +402,15 @@ def add():
     if isinstance(arg1, int):
        if isinstance(arg2, int):
            opstack.append(arg1+arg2)
-           print("Successfully added")
+          
            return
     if isinstance(arg1, float):
        if isinstance(arg2, float):
            opstack.append(arg1+arg2)
-           print("Successfully added")
+           
            return
-    else:
-        print("Incorrect type")
+    
+       
          
     opPush(arg2)
     opPush(arg1)
@@ -79,7 +418,6 @@ def add():
         
 def sub():
     if len(opstack) < 2:
-        print("Not enough Arguements")
         return False
     arg2 = opstack.pop()
     arg1 = opstack.pop()
@@ -87,15 +425,12 @@ def sub():
     if isinstance(arg1, int):
        if isinstance(arg2, int):
            opstack.append(arg1-arg2)
-           print("Successfully Subtracted")
            return
     if isinstance(arg1, float):
        if isinstance(arg2, float):
            opstack.append(arg1-arg2)
-           print("Successfully Subtracted")
            return
-    else:
-        print("Incorrect type")
+    
         
     opPush(arg2)
     opPush(arg1)
@@ -103,7 +438,6 @@ def sub():
 
 def mul():
     if len(opstack) < 2:
-        print("Not enough Arguements")
         return False
     arg1 = opstack.pop()
     arg2 = opstack.pop()
@@ -111,15 +445,12 @@ def mul():
     if isinstance(arg1, int):
        if isinstance(arg2, int):
            opstack.append(arg1*arg2)
-           print("Successfully Multiplied")
            return
     if isinstance(arg1, float):
        if isinstance(arg2, float):
            opstack.append(arg1*arg2)
-           print("Successfully Multiplied")
            return
-    else:
-        print("Incorrect type")
+    
         
     opPush(arg2)
     opPush(arg1)
@@ -128,7 +459,6 @@ def mul():
 def div():
     
     if len(opstack) < 2:
-        print("Not enough Arguements")
         return False
     arg2 = opstack.pop()
     arg1 = opstack.pop()
@@ -136,15 +466,12 @@ def div():
     if isinstance(arg1, int):
        if isinstance(arg2, int):
            opstack.append(arg1/arg2)
-           print("Successfully Divided")
            return
     if isinstance(arg1, float):
        if isinstance(arg2, float):
            opstack.append(arg1/arg2)
-           print("Successfully Divided")
            return
-    else:
-        print("Incorrect type")
+    
         
     opPush(arg2)
     opPush(arg1)
@@ -153,7 +480,6 @@ def div():
 def mod():
     
     if len(opstack) < 2:
-        print("Not enough Arguements")
         return False
     arg2 = opstack.pop()
     arg1 = opstack.pop()
@@ -161,16 +487,12 @@ def mod():
     if isinstance(arg1, int):
        if isinstance(arg2, int):
            opstack.append(arg1 % arg2)
-           print("Successfully Modded")
            return
     if isinstance(arg1, float):
        if isinstance(arg2, float):
            opstack.append(arg1 % arg2)
-           print("Successfully Modded")
            return
-    else:
-        print("Incorrect type")
-        
+          
         
     opPush(arg2)
     opPush(arg1)
@@ -178,13 +500,13 @@ def mod():
     
 def eq():
     if len(opstack) < 2:
-        print("Not enough Arguements")
+        
         return False
     arg1 = opstack.pop()
     arg2 = opstack.pop()
     
     if arg1 == arg2:
-        print("They are Equal")
+        
         opPush(True)
     else:
         opPush(False)
@@ -193,13 +515,12 @@ def eq():
 
 def lt():
     if len(opstack) < 2:
-        print("Not enough Arguements")
+       
         return False
     arg1 = opstack.pop()
     arg2 = opstack.pop()
     
     if arg1 > arg2:
-        print("is Less then")
         opPush(True)
         
         return True
@@ -210,13 +531,11 @@ def lt():
     
 def gt():
     if len(opstack) < 2:
-        print("Not enough Arguements")
         return False
     arg1 = opstack.pop()
     arg2 = opstack.pop()
     
     if arg1 < arg2:
-        print("is Greater then")
         opPush(True)
         
     else:
@@ -228,11 +547,9 @@ def length():
     temp = opPop()
     if isinstance(temp, str):
         temp = len(temp)
-        print("Length = ", temp - 2)
         opPush(temp - 2)
         return 
     else:
-        print("Was not a String")
         return False
     
         
@@ -246,7 +563,7 @@ def get():
         opPush(value)
         return True
     else:
-        print("Was not a String")
+        
         return False
     
     
@@ -261,10 +578,10 @@ def getinterval():
         str_split = temp[start_index: (start_index+num_char)]
         str_split = "(" +  str_split + ")"
         opPush(str_split)
-        print("successful getInterval")
+        
         return 
     else:
-        print("Was not a String")
+        
         opPush(temp)
         return False
     
@@ -282,8 +599,8 @@ def put():
             updated = "(" + updated + ")"
            
             for i in range(len(opstack)):
-                if opstack[i] == temp:
-                    opstack[i] = updated
+                if opstack[i] == og:
+                    del opstack[i]
            
             for d in dictstack:
                 for key, value in d.items():
@@ -291,16 +608,16 @@ def put():
                         d[key] = updated
             opPush(updated)  
         else:
-            print("Error: Invalid input")
+            print
             return False
     else:
-        print("Error: Not enough items in the opstack")
+        print
         return False
 
 
 def dup():
     if not opstack:
-        print("Error: opstack is empty")
+        
         return False
     temp1 = opPop()
     temp2 = temp1
@@ -320,11 +637,12 @@ def pop():
 
 def clear():
     opstack.clear()
+    dictstack.clear()
     
 
 def exch():
     if not opstack:
-        print("Error: opstack is empty")
+        
         return False
     temp1 = opPop()
     temp2 = opPop()
@@ -333,22 +651,27 @@ def exch():
 
 
 def roll():
-    num_temp = opPop()
-    move = opPop()
-    
-    
-    temp_move = opstack.index(num_temp) + move
-    opstack.remove(num_temp)
-    
-    if temp_move > len(opstack):
-        opstack.append(num_temp)
-    else:
-        opstack.insert(temp_move, num_temp)
+    x = int(opPop())  
+    y = int(opPop())  
+
+    if y > len(opstack):
+        return False
+    x = x % y if x > 0 else -(-x % y)
+    segment = opstack[-y:]
+
+    if x > 0: 
+        segment = segment[-x:] + segment[:-x]
+    else:  
+        x = -x 
+        segment = segment[x:] + segment[:x]
+    opstack[-y:] = segment
+
+
     
         
 def stack():
     if not opstack:
-        print("stack is empty")
+        
         return False
     else:
         for temp in reversed(opstack):
@@ -356,18 +679,18 @@ def stack():
     
     
 def psDict():
-    if not opstack:
-        return False
+    if len(opstack) > 0:
+        opPop()
+        opPush({})
     else:
-        temp = opstack.pop()
-        temp_dic = {}
-        opstack.append(temp_dic)
-        
-        
+        return False
+    
+    
 def begin():
-    temp = opstack.pop()
-    if isinstance(temp, dict):
-        dictPush(temp)
+    if len(opstack) > 0:
+        temp = opPop()
+        
+        
     else:
         return False
     
@@ -376,7 +699,7 @@ def end():
     temp = dictstack.pop()
     if isinstance(temp, dict):
         return True
-    
+
 
 def psDef():
     temp_value = opstack.pop()
@@ -392,27 +715,25 @@ def psDef():
 # Copy this to your HW5.py file>
 def interpreter(s): # s is a string
     interpretSPS(parse(tokenize(s)))
+    clear()
     
-#Test Section -------------------------
-    
-def testPut():
-    opPush("(This is a test _)")
-    dup()
-    opPush("/s")
-    exch()
-    psDef()
-    dup()
-    opPush(15)
-    opPush(48)
-    put()
-    if lookup("s") != "(This is a test 0)" or opPop() != "(This is a test 0)":
-        return False
-    return True
-    
-    
- 
 
-#This is where i will test the functions
-#print(testPut())
 
-print(interpreter(input1))
+#print(tokenize(input2))
+#print(parse(tokenize(input2)))
+
+
+
+interpreter(input1)
+clear()
+interpreter(input2)
+clear()
+interpreter(input3)
+clear()
+interpreter(input4)
+clear()
+interpreter(input5)
+clear()
+interpreter(input6)
+
+
